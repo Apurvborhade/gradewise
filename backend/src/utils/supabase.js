c95f1755727a3d0693e1bd16a700c6950d990825
Apurvborhade <apurvaborhadee@gmail.com>
2025-03-06 09:02:13 +0530

    supabase config

--- backend/src/utils/supabase.js
+++ backend/src/utils/supabase.js
@@ -1,3 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient('https://xoibjeqtqmoczyekzmnx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvaWJqZXF0cW1vY3p5ZWt6bW54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExODI0NjYsImV4cCI6MjA1Njc1ODQ2Nn0.5DRl7eLdTGY6nbiAxnD5y02oGJwrP8SASD8YHMot6WU');
+
+export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET);
