b7cbe769077c2d4bfe9d4f4acf51f588e15109b9
Apurvborhade <apurvaborhadee@gmail.com>
2025-03-26 22:10:04 +0530

    production cookie error (fix attempt 2)

--- frontend/src/app/features/users/usersApi.js
+++ frontend/src/app/features/users/usersApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const userApi = createApi({
     reducerPath: 'userApi',
-    baseQuery: fetchBaseQuery({ baseUrl: `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth`, credentials: 'include' }),
+    baseQuery: fetchBaseQuery({ baseUrl: `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth`, credentials: 'include', }),
     tagTypes: ['User'],
     endpoints: (build) => ({
         userSignup: build.mutation({
