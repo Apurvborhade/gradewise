2abdf3827341045bd2a239b78b8d33297418080e
Apurvborhade <apurvaborhadee@gmail.com>
2025-03-31 12:18:23 +0530

    landing page error (fix)

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,7 +2,8 @@
 import React from "react";
 import Link from "next/link";
 
-const FeedbackAI = () => {
+
+export default function Home() {
   return (
     <>
       <link
@@ -463,42 +464,7 @@ const FeedbackAI = () => {
           </div>
         </footer>
       </div>
-=======
-// src/app/page
-// "user client";
-
-
-// import Navigation from './landing/Navigation';
-// import HeroSection from './landing/HeroSection';
-// import ProblemsSection from './landing/ProblemsSection';
-// import SolutionsSection from './landing/SolutionsSection';
-// import ImpactSection from './landing/ImpactSection';
-// import TechStackSection from './landing/TechStackSection';
-// import DemoSection from './landing/DemoSection';
-// import TeamSection from './landing/TeamSection';
-// import ContactSection from './landing/ContactSection';
-// import Footer from './landing/Footer';
-// import ClientScripts from './landing/ClientScripts';
-
-export default function Home() {
-  return (
-    <>
-      {/* <Navigation /> */}
-      <main>
-        {/* <HeroSection />
-        <ProblemsSection />
-        <SolutionsSection /> 
-        <ImpactSection />
-        <TechStackSection />
-        <DemoSection />
-        <TeamSection />
-        <ContactSection /> */}
-      </main>
-      {/* <Footer />
-      <ClientScripts /> */}
-
     </>
   );
 };
 
-export default FeedbackAI;
